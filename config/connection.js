e5837f22cccd7b81c66eea48c2914c3e9d60407b
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-19 15:42:03 +0000

    Adding comments

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
+// Requiring dotenv npm 
 require('dotenv').config();
+// Requiring Sequelize npm 
 const Sequelize = require('sequelize');
 
+// Creating the connection to the database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
