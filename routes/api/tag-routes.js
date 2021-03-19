01c19cb8a6df1041ad630ec6bb37e1d087ba6381
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-19 15:20:59 +0000

    Adding async to put and post routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create(req.body);
@@ -43,7 +43,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body, {
