98e8417ae732b9b0919c0448459c8875cb1959a6
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-19 15:52:45 +0000

    Adding comments

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,9 +1,12 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// import our database connection from connection.js
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
+class ProductTag extends Model { }
 
+// set up fields and rules for Product model
 ProductTag.init(
   {
     id: {
@@ -28,6 +31,7 @@ ProductTag.init(
     },
   },
   {
+    // Link to database connection
     sequelize,
     timestamps: false,
     freezeTableName: true,
